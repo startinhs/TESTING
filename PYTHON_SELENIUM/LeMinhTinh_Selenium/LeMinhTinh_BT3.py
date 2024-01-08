a8680770dc0e0c84a1123072747b6e3f24ba9f55
startinh <tinhcv181@gmail.com>
2024-01-08 23:19:00 +0700

    up selenium

---
+++ PYTHON_SELENIUM/LeMinhTinh_Selenium/LeMinhTinh_BT3.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
+driver = webdriver.Chrome()
+driver.get('https://vnexpress.net/')
+results = driver.find_elements(By.CSS_SELECTOR, 'article.item-news')
+driver.implicitly_wait(15)
+for re in results:
+    # try:
+    text = re.find_element(By.TAG_NAME, 'h3').text
+    print(text)
+    print("*******************************************")
+    # except NoSuchElementException:
+    #     print("Loi!")
+    #     print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+driver.close()
